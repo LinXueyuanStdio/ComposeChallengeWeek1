ddca9420e7ebea467f80017f8bad82be978578f5
兮尘 <761516186@qq.com>
2021-03-02 14:53:29 +0800

    Update PuppyDetails.kt

--- app/src/main/java/com/example/androiddevchallenge/ui/PuppyDetails.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/PuppyDetails.kt
@@ -22,7 +22,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CutCornerShape
